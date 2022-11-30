be51adcaad00350efc13bc1dbf9cf316a3ff88ca
Annielle <anniellemedeiros@gmail.com>
2022-11-30 09:33:28 -0300

    feat: basic project

---
+++ mannagent/src/main/java/com/guild/mannagent/MannagentApplication.java
@@ -0,0 +1,13 @@
+package com.guild.mannagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MannagentApplication.class, args);
+	}
+
+}
