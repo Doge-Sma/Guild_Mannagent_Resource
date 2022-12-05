065ae0b409cce81af0e579fe949d1b8a2e483368
Annielle <anniellemedeiros@gmail.com>
2022-12-05 18:17:12 -0300

    add update tamanho do inventario service

--- mannagent/src/main/java/com/guild/mannagent/services/InventarioService.java
+++ mannagent/src/main/java/com/guild/mannagent/services/InventarioService.java
@@ -1,8 +1,28 @@
 package com.guild.mannagent.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guild.mannagent.entity.Inventario;
+import com.guild.mannagent.repository.InventarioRepository;
+
+import java.util.Optional;
+
+@Service
 public class InventarioService {
+    @Autowired
+    private InventarioRepository inventarioRepository;
     
     //Melhorar Inventario
+    public Inventario updateInventario (Inventario inventario, Long inventarioId){
+        Optional<Inventario> inventarioTemp = this.inventarioRepository.findById(inventarioId);
+            if(inventarioTemp.isPresent()){
+                Inventario i = inventarioTemp.get();
+                i.setTamanho(inventario.getTamanho()+1);
+                return this.inventarioRepository.save(i);
+            }
+            return null;
+    }
 
 
 }
