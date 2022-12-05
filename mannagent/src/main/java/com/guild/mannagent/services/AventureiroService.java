31ad44b58afed85cb39486f1944184bc6a10a90a
Annielle <anniellemedeiros@gmail.com>
2022-12-05 16:43:46 -0300

    add update aventureiro service

--- mannagent/src/main/java/com/guild/mannagent/services/AventureiroService.java
+++ mannagent/src/main/java/com/guild/mannagent/services/AventureiroService.java
@@ -4,9 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guild.mannagent.repository.AventureiroRepository;
+import com.guild.mannagent.entity.Aventureiro;
+import java.util.Optional;
 
 @Service
 public class AventureiroService {
     @Autowired
     private AventureiroRepository aventureiroRepository;
+
+/* cadastrar aventureiro
+atualizar aventureiro
+listar aventureiro */
+
+public Aventureiro createAventureiro (Aventureiro aventureiro){
+    return this.aventureiroRepository.save(aventureiro);
+}
+
+public Aventureiro updateAventureiro (Aventureiro aventureiro, Long aventureiroId){
+    Optional<Aventureiro> aventureiroTemp = this.aventureiroRepository.findById(aventureiroId);
+        if(aventureiroTemp.isPresent()){
+            Aventureiro a = aventureiroTemp.get();
+            a.setNome(aventureiro.getNome());
+            a.setClasse(aventureiro.getClasse());
+            a.setCarteira(aventureiro.getCarteira());
+            a.setRank(aventureiro.getRank());
+            return this.aventureiroRepository.save(a);
+        }
+        return null;
+
+}
 }
