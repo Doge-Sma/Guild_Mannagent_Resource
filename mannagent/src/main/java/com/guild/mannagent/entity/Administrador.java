04027c1e4a79dbddaa9a10a2e1a03dfb6f788de6
Annielle <anniellemedeiros@gmail.com>
2022-12-13 21:32:55 -0300

    update entidy validation adm

--- mannagent/src/main/java/com/guild/mannagent/entity/Administrador.java
+++ mannagent/src/main/java/com/guild/mannagent/entity/Administrador.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,10 +32,13 @@ public class Administrador {
     @Column(name = "id_Administrador")
     private Long id;
     @Column
+    @NotBlank
     private String nome;
     @Column
+    @NotBlank
     private String senha;
     @Column
+    @Email
     private String email;
 
     @ManyToMany
